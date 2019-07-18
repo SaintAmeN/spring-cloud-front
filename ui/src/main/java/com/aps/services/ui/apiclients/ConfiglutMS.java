fe4e16e167b024b80503f7601e4e5deee87308ba
Ania Seta <a.seta@apsystems.tech>
2019-07-18 11:23:39 +0200

    Clean up.

--- ui/src/main/java/com/aps/services/ui/apiclients/ConfiglutMS.java
+++ ui/src/main/java/com/aps/services/ui/apiclients/ConfiglutMS.java
@@ -1,16 +1,12 @@
 package com.aps.services.ui.apiclients;
 
 import com.aps.services.apiclients.IConfiglutMS;
-import com.aps.services.model.dto.OwnPageImpl;
+import com.aps.services.model.pagination.OwnPageImpl;
 import com.aps.services.model.dto.configlut.response.RadarResponseDto;
-import com.aps.services.ui.util.CustomPageImpl;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @FeignClient("configlut-ms")
 public interface ConfiglutMS extends IConfiglutMS {
